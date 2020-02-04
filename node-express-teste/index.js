6b61bfdd2a8131b114a2c5be0fffe5d3d4abafba
rodrigoelyel <rodrigoelyelcb@gmail.com>
2020-02-04 15:57:58 -0300

    REST

--- node-express-teste/index.js
+++ node-express-teste/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const morgan = require('morgan')
 const http = require('http')
 const express = require('express')
+const rotasPizzas = require('./rotas/pizza')
 
 
 const porta = 3000
@@ -9,12 +10,10 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.use((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Hello express app</h1>')
-})
+app.use('/pizzas', rotasPizzas)
 
 const servidor = http.createServer(app)
 
